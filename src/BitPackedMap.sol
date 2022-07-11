cf870190dad3a5ffc5d853751b38973f47435378
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-11 16:10:21 -0700

    General function layout

---
+++ src/BitPackedMap.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract BitPackedMap {
+    // Given a value for a square, return the corresponding fill rgb hex string
+    function getFillFromBits(uint256) internal view returns (string memory) {
+
+    }
+
+    // Returns svg string corresponding to a 32 byte bitmap
+    function renderSvg(bytes32 bitmap) internal view returns (string memory) {
+        // string memory svgString = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 8 8">';
+
+        
+    }
+
+    // Returns svg string corresponding to a specific tokenId
+    function tokenSvg(uint256 tokenId) public view returns (string memory) {
+
+    }
+}
