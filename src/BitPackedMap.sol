1c28b8b5806efe61ab61b6686051159422f94509
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-11 17:01:22 -0700

    getSquareFromMap

--- src/BitPackedMap.sol
+++ src/BitPackedMap.sol
@@ -7,11 +7,20 @@ contract BitPackedMap {
 
     }
 
+    // Returns bits corresponding to a square from the bitmap
+    // TODO: Make visibility internal
+    function getSquareFromMap(bytes32 bitmap, uint256 index) public pure returns (uint256 square) {
+        assembly {
+            let shift := mul(index, 4)
+            square := shr(shift, and(shl(shift, shr(0xFC, not(0))), bitmap))
+        }
+    }
+
     // Returns svg string corresponding to a 32 byte bitmap
-    function renderSvg(bytes32 bitmap) internal view returns (string memory) {
+    // TODO: Make visibility internal
+    function renderSvg(bytes32 bitmap) public view returns (string memory) {
         // string memory svgString = '<?xml version="1.0" encoding="UTF-8" standalone="no"?><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 8 8">';
 
-        
     }
 
     // Returns svg string corresponding to a specific tokenId
--- test/BitPackedMap.t.sol
+++ test/BitPackedMap.t.sol
@@ -2,11 +2,17 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/BitPackedMap.sol";
 
-contract ContractTest is Test {
-    function setUp() public {}
+contract BitPackedMapTest is Test {
+    BitPackedMap bpm;
 
-    function testExample() public {
-        assertTrue(true);
+    function setUp() public {
+        bpm = new BitPackedMap();
+    }
+
+    function testGetSquareFromMap() public {
+        uint256 square = bpm.getSquareFromMap(0x7624778dedc75f8b322b9fa1632a610d40b85e106c7d9bf0e743a9ce291b9c63, 39);
+        assertEq(square, 0x06);
     }
 }
