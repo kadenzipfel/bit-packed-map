330f64833c5d8aaf42a9e73178c192f8a3868839
kadenzipfel <kaden.zipfel@hotmail.com>
2022-07-11 21:14:42 -0700

    Label private methods

--- src/BitPackedMap.sol
+++ src/BitPackedMap.sol
@@ -21,7 +21,7 @@ contract BitPackedMap {
                 b := div(b, 2)
             }
         }
-        return string(abi.encodePacked("#", uintToHexString(r), uintToHexString(g), uintToHexString(b)));
+        return string(abi.encodePacked("#", _uintToHexString(r), _uintToHexString(g), _uintToHexString(b)));
     }
 
     // Returns bits corresponding to a square from the bitmap
@@ -40,14 +40,14 @@ contract BitPackedMap {
 
         uint256 y = 0;
         for (uint i; i < 64; i += 8) {
-            pixelRow[0] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i)), '" x="0" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[1] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 1)), '" x="1" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[2] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 2)), '" x="2" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[3] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 3)), '" x="3" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[4] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 4)), '" x="4" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[5] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 5)), '" x="5" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[6] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 6)), '" x="6" y="', toString(y), '" width="1" height="1" />'));
-            pixelRow[7] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 7)), '" x="7" y="', toString(y), '" width="1" height="1" />'));
+            pixelRow[0] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i)), '" x="0" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[1] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 1)), '" x="1" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[2] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 2)), '" x="2" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[3] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 3)), '" x="3" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[4] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 4)), '" x="4" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[5] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 5)), '" x="5" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[6] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 6)), '" x="6" y="', _toString(y), '" width="1" height="1" />'));
+            pixelRow[7] = string(abi.encodePacked('<rect fill="', _getFillFromSquare(_getSquareFromMap(bitmap, i + 7)), '" x="7" y="', _toString(y), '" width="1" height="1" />'));
             
             svgString = string(abi.encodePacked(svgString, pixelRow[0], pixelRow[1], pixelRow[2], pixelRow[3], pixelRow[4], pixelRow[5], pixelRow[6], pixelRow[7]));
 
@@ -71,7 +71,7 @@ contract BitPackedMap {
     }
 
     // SOURCE: https://etherscan.io/address/0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63#code
-    function uintToHexDigit(uint8 d) internal pure returns (bytes1) {
+    function _uintToHexDigit(uint8 d) private pure returns (bytes1) {
         if (0 <= d && d <= 9) {
             return bytes1(uint8(bytes1('0')) + d);
         } else if (10 <= uint8(d) && uint8(d) <= 15) {
@@ -81,7 +81,7 @@ contract BitPackedMap {
     }
 
     // SOURCE: https://etherscan.io/address/0x8d04a8c79ceb0889bdd12acdf3fa9d207ed3ff63#code
-    function uintToHexString(uint a) internal pure returns (string memory) {
+    function _uintToHexString(uint a) private pure returns (string memory) {
         uint count = 0;
         uint b = a;
         while (b != 0) {
@@ -91,7 +91,7 @@ contract BitPackedMap {
         bytes memory res = new bytes(count);
         for (uint i=0; i<count; ++i) {
             b = a % 16;
-            res[count - i - 1] = uintToHexDigit(uint8(b));
+            res[count - i - 1] = _uintToHexDigit(uint8(b));
             a /= 16;
         }
         
@@ -105,7 +105,7 @@ contract BitPackedMap {
     }
 
     // SOURCE: https://github.com/OpenZeppelin/openzeppelin-contracts/blob/3210a8653b66726114226ee5153bbcf59b0475bd/contracts/utils/Strings.sol#L16
-    function toString(uint256 value) internal pure returns (string memory) {
+    function _toString(uint256 value) private pure returns (string memory) {
         // Inspired by OraclizeAPI's implementation - MIT licence
         // https://github.com/oraclize/ethereum-api/blob/b42146b063c7d6ee1358846c198246239e9360e8/oraclizeAPI_0.4.25.sol
 
